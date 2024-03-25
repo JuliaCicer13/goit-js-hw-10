5e3aa47853ba7aa7225759fa55ba19d6e2849cf7
JuliaCicer13 <137887271+JuliaCicer13@users.noreply.github.com>
2024-03-26 00:32:53 +0200

    add

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,7 +1,63 @@
-import flatpickr from "flatpickr"
-import "flatpickr/dist/flatpickr.min.css"
+
+import flatpickr from "flatpickr";
+
+import iziToast from "izitoast";
+
+import { convertMs } from "./timeUtils";
 
 
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    const currentDate = new Date();
+    if (selectedDate < currentDate) {
+     
+      iziToast.error({
+        title: 'Error',
+        message: 'Please choose a date in the future'
+      });
+      document.getElementById('startButton').disabled = true;
+    } else {
+    
+      document.getElementById('startButton').disabled = false;
+    }
+  },
+};
 
-const inputElement = document.createElement("input");
-const buttonName = document.createElement("button");
+
+flatpickr("#datetime-picker", options);
+
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+
+document.getElementById('startButton').addEventListener('click', function() {
+  const selectedDate = new Date(document.getElementById('datetime-picker').value);
+  const countdownInterval = setInterval(function() {
+    const currentTime = new Date();
+    const timeDifference = selectedDate - currentTime;
+    if (timeDifference <= 0) {
+      clearInterval(countdownInterval);
+      updateTimerDisplay(0, 0, 0, 0);
+      document.getElementById('startButton').disabled = true;
+      document.getElementById('datetime-picker').disabled = true;
+    } else {
+      const { days, hours, minutes, seconds } = convertMs(timeDifference);
+      updateTimerDisplay(days, hours, minutes, seconds);
+    }
+  }, 1000);
+});
+
+
+function updateTimerDisplay(days, hours, minutes, seconds) {
+  document.querySelector('[data-days]').textContent = addLeadingZero(days);
+  document.querySelector('[data-hours]').textContent = addLeadingZero(hours);
+  document.querySelector('[data-minutes]').textContent = addLeadingZero(minutes);
+  document.querySelector('[data-seconds]').textContent = addLeadingZero(seconds);
+}
