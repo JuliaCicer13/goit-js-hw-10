ad8d3ef12e40c8b3820b9857d4d721d1be1e39cb
JuliaCicer13 <137887271+JuliaCicer13@users.noreply.github.com>
2024-03-25 23:52:40 +0200

    Update 1-timer.js

---
+++ src/js/1-timer.js
@@ -0,0 +1,7 @@
+import flatpickr from "flatpickr"
+import "flatpickr/dist/flatpickr.min.css"
+
+
+
+const inputElement = document.createElement("input");
+const buttonName = document.createElement("button");
