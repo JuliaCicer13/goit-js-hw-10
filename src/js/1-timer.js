522b27a91c3e53cad8b8265a3cf773f77e8c12ea
JuliaCicer13 <137887271+JuliaCicer13@users.noreply.github.com>
2024-12-23 02:11:26 +0200

    g

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,77 +1,32 @@
 
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+const dataButton = document.querySelector('button[data-start]');
 
+let userSelectedDate = new Data();
 
-let intervalId = null;
-const startButton = document.querySelector('[data-start]');
-const dateTimePicker = document.getElementById('datetime-picker');
-const clockFace = document.querySelector('.timer');
+console.log(userSelectedDate);
 
-
-
-let initTime = 0;
-
-
+flatpickr(selector, options);
+ 
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    const selectedDate = selectedDates[0];
-    const currentDate = new Date();
-    if (selectedDate < currentDate) {
-      iziToast.error({ message: 'Please choose a date in the future' });
-      startButton.disabled = true;
-    } else {
-      startButton.disabled = false;
-      initTime = selectedDate.getTime(); // Update initTime with selected date
-    }
+    console.log(selectedDates[0]);
   },
 };
 
- startButton.disabled = true;
-
-flatpickr(dateTimePicker, options);
+function flatpickr() { };
 
 
+dataButton.addEventListener("click", handleTimer);
+
+function handleTimer(event) {
+  event
 
 
-startButton.addEventListener('click', function () {
 
-  if (!dateTimePicker.value) {
- 
-    iziToast.error({ message: 'Please choose a date' });
-    return;
-  }
-
- 
-  intervalId = setInterval(() => {
-    const currentTime = Date.now();
-    const diff = initTime - currentTime;
-    if (diff <= 0) {
-      clearInterval(intervalId);
-      clockFace.textContent = '00:00:00:00';
-      startButton.disabled = false; 
-      return;
-    }
-    const time = formatTime(diff);
-    clockFace.textContent = time;
-  }, 1000);
-});
-
-const formatTime = milliseconds => {
-  const days = Math.floor(milliseconds / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((milliseconds % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((milliseconds % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((milliseconds % (1000 * 60)) / 1000);
-  return [
-    days.toString().padStart(2, '0'),
-    hours.toString().padStart(2, '0'),
-    minutes.toString().padStart(2, '0'),
-    seconds.toString().padStart(2, '0'),
-  ].join(':');
 }
